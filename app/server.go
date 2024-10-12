359f0c95bdb11242dcf4cf47b6601b2bcdb7cf17
MdArifulIslam21 <arifuldesigner2014@gmail.com>
2024-10-12 22:37:39 +0600

    stage 3 complete

--- app/server.go
+++ app/server.go
@@ -1,33 +1,40 @@
 package main
-
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
-
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
 	// Uncomment this block to pass the first stage
 	//
-	l, err := net.Listen("tcp", "0.0.0.0:9092")
+	listener, err := net.Listen("tcp", ":9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
+	connection, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	println("New Connection")
+	buff := make([]byte, 1024)
+	_, err = connection.Read(buff)
+	if err != nil {
+		println("Error while reading message: ", err.Error())
+		os.Exit(1)
+	}
 
-	defer conn.Close()
-	conn.Read(make([]byte, 1024))
-	conn.Write([]byte{0, 0, 0, 0, 0, 0, 0, 7})
+	fmt.Printf("Received message %v (%d)", buff[8:12], int32(binary.BigEndian.Uint32(buff[8:12])))
+	resp := make([]byte, 8)
+	copy(resp, []byte{0, 0, 0, 0})
+	copy(resp[4:], buff[8:12])
+	connection.Write(resp)
+	connection.Close()
 }
